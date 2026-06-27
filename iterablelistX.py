4c703335d577bba6707e36cc3b8790d7976149e7
samruddhimandage <samruddhimandage@gmail.com>
2026-06-27 18:32:50 +0530

    change in code

--- iterablelistX.py
+++ iterablelistX.py
@@ -1,5 +1,5 @@
 Data = [11,21,51,101]      #list
 print("Data from list is :",Data)
 
-for no in range(len(Data)):
-    print(Data[no])
+for no in range(len(Data)):         #here no is index value
+    print(Data[no])                 #accessing data using index value
