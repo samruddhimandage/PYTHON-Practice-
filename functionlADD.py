3d14684dfc85f0d5f2a8eb4856beaae78d6c09c5
samruddhimandage <samruddhimandage@gmail.com>
2026-07-03 20:57:37 +0530

    new code added

---
+++ functionlADD.py
@@ -0,0 +1,6 @@
+Add = lambda NO1 ,NO2 : NO1 + NO2
+
+ret =Add(10,11)            #ret = 10 + 11    (done by compiler therefore interpreter will have only this line to execute no need to interprete the actual functio)
+
+print ("Addition is :",ret )
+
