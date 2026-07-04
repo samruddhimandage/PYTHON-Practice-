ef78d0351cafaa59ad786daa1032151588636432
samruddhimandage <samruddhimandage@gmail.com>
2026-07-04 20:02:24 +0530

    new code added

---
+++ time_calcuation3.py
@@ -0,0 +1,25 @@
+ 
+import time
+
+def factorial(No):
+    fact =1
+    for i in range(1,No+1):
+        fact=fact*i
+        
+    return fact
+        
+def main():
+    value=int(input("enter a number :"))
+    
+    start_time=time.time()
+    
+    ret = factorial(value)
+    
+    end_time=time.time()
+    
+    print(f"factorial of {value} is :{ret}")    #formated printing
+    
+    print(f"time requreed :{end_time-start_time}")
+    
+if __name__=="__main__":
+    main()
