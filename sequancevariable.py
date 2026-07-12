d16f1b98ed0e1c01aa7e138f05b627ce02310f61
samruddhimandage <samruddhimandage@gmail.com>
2026-07-12 21:05:04 +0530

    new code added

--- sequancevariable.py
+++ sequancevariable.py
@@ -2,7 +2,7 @@ data1 = [10,20,30,40]                 #list (kind of array)
 data2 = (10,20,30,40)                 #tuple
 data3 = {10,20,30,40}                 #set
 data4 ={"a":10,"b":20,"c":30,"d":40}  #dict
-data5 ="maharastra"                   #str
+data5 ="Ganesh..."                   #str
 
 print(type(data1))
 print(type(data2))
