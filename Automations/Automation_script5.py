fdbf75fa93690498909bca6deab1f1aeaf515c84
samruddhimandage <samruddhimandage@gmail.com>
2026-07-19 18:22:40 +0530

    new code added

---
+++ Automations/Automation_script5.py
@@ -0,0 +1,33 @@
+import sys
+
+def main():
+    
+    print("-------------------------------------------------------------")
+    print("Welcome to marvellous Automation script")
+    print("-------------------------------------------------------------")
+
+    if(len(sys.argv)==2):
+        
+        if(sys.argv[1]=="--h" or sys.argv[1]=="--H"):
+            print("This Automation script is use to travel the directory")
+            print("for better usage please cheack --u flag")
+            
+        elif(sys.argv[1]=="--u" or sys.argv[1]=="--U"):
+            print("Please execute the script as ")
+            print("python Filename.py DirectoryName ")
+            print("Directory name should be absolute path")
+            
+        DirectoryName=sys.argv[1]
+        print("directory name is :",DirectoryName)
+        
+    else:
+        print("invlide input")
+        print("please use --h or --u for more info")
+    
+    print("-------------------------------------------------------------")
+    print("Thank you for using marvellous Automation script")
+    print("-------------------------------------------------------------")
+
+if __name__=="__main__":
+    main()
+    
